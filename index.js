8667ce06e03c9dea5ea1182beb961ff375f599de
Adam Lofting <contactme@adamlofting.com>
2016-04-29 10:19:14 +0100

    setup

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var csv = require('csv');
+
+
+// load csv file from a folder (that doesn't get committed to the repo)
+
+// build a list of all nodes
+
+// add attributes to these where known
+
+// build a list of edges
